0336ef1e24327316b11281176ddc19209a2285e3
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-02 09:52:18 -0800

    Add expectation for average age to eq 0 if there are no actors

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe Movie do
     it 'finds the average age of the actors' do
       universal = Studio.create!(name: "Universal Studios", location: "Hollywood")
       jurassic_park = universal.movies.create!(title: "Jurassic Park", creation_year: "1993", genre: "Dinosaurs")
+      harry_potter = universal.movies.create!(title: "Harry Potter and the Sorceror's Stone", creation_year: "1998", genre: "Fantasy")
 
       jeff = Actor.create!(name: "Jeff Goldblum", age: 70)
       laura = Actor.create!(name: "Laura Dern", age: 55)
@@ -38,6 +39,7 @@ RSpec.describe Movie do
       MovieActor.create!(movie_id: jurassic_park.id, actor_id: sam.id)
 
       expect(jurassic_park.average_age_of_actors).to eq(66.67)
+      expect(harry_potter.average_age_of_actors).to eq(0)
     end 
   end
 end
