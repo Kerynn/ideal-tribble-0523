6d9ab2880812b4107fb409391e7026e0778dd513
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-02 09:44:50 -0800

    Complete us 3 add a form to add an actor to a movie

--- spec/features/movie_show_page.rb
+++ spec/features/movie_show_page.rb
@@ -49,4 +49,71 @@ RSpec.describe 'Movies' do
       expect(page).to have_content("Average Age of Actors: 66.67")
     end
   end
+
+  describe "adding an actor to a movie" do
+    describe "when I visit the movie show page" do
+      it 'does not show actors that are not part of the movie' do
+        universal = Studio.create!(name: "Universal Studios", location: "Hollywood")
+        jurassic_park = universal.movies.create!(title: "Jurassic Park", creation_year: "1993", genre: "Dinosaurs")
+  
+        jeff = Actor.create!(name: "Jeff Goldblum", age: 70)
+        laura = Actor.create!(name: "Laura Dern", age: 55)
+        sam = Actor.create!(name: "Sam Neill", age: 75)
+        samuel = Actor.create!(name: "Samuel L. Jackson", age: 74)
+        wayne = Actor.create!(name: "Wayne Knight", age: 67)
+
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: jeff.id)
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: laura.id)
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: sam.id)
+  
+        visit "/movies/#{jurassic_park.id}"
+
+        expect(page).to_not have_content(samuel.name)
+        expect(page).to_not have_content(wayne.name)
+      end
+
+      it 'will have a form to add an actor to this movie' do
+        universal = Studio.create!(name: "Universal Studios", location: "Hollywood")
+        jurassic_park = universal.movies.create!(title: "Jurassic Park", creation_year: "1993", genre: "Dinosaurs")
+  
+        jeff = Actor.create!(name: "Jeff Goldblum", age: 70)
+        laura = Actor.create!(name: "Laura Dern", age: 55)
+        sam = Actor.create!(name: "Sam Neill", age: 75)
+        samuel = Actor.create!(name: "Samuel L. Jackson", age: 74)
+
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: jeff.id)
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: laura.id)
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: sam.id)
+  
+        visit "/movies/#{jurassic_park.id}"
+
+        fill_in :actor_id, with: samuel.id
+        expect(page).to have_button("Add Actor to Movie")
+      end
+
+      it 'will show the new actor listed on the movie show page once submit the form' do
+        universal = Studio.create!(name: "Universal Studios", location: "Hollywood")
+        jurassic_park = universal.movies.create!(title: "Jurassic Park", creation_year: "1993", genre: "Dinosaurs")
+  
+        jeff = Actor.create!(name: "Jeff Goldblum", age: 70)
+        laura = Actor.create!(name: "Laura Dern", age: 55)
+        sam = Actor.create!(name: "Sam Neill", age: 75)
+        samuel = Actor.create!(name: "Samuel L. Jackson", age: 74)
+
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: jeff.id)
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: laura.id)
+        MovieActor.create!(movie_id: jurassic_park.id, actor_id: sam.id)
+  
+        visit "/movies/#{jurassic_park.id}"
+
+        expect(page).to_not have_content(samuel.name)
+
+        fill_in :actor_id, with: samuel.id
+        click_button "Add Actor to Movie"
+
+        expect(current_path).to eq("/movies/#{jurassic_park.id}")
+        expect(page).to have_content(samuel.name)
+      end
+    end
+  end
 end
